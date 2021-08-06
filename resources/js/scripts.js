2b98e9716a716a03fee0e501ca0512ea244d1b7a
juan9900 <juanluislauretta@gmail.com>
2021-08-06 12:37:02 -0400

    Listo responsive

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,26 +1,4 @@
-var g_interval;
-new fullpage("#fullpage", {
-  afterLoad: function (origin, destination, direction) {
-    clearInterval(g_interval);
+if ($(window).width() < 480) {
+}
 
-    // 1000 milliseconds lapse
-    const lapse = 1000;
-
-    if (destination.item.querySelectorAll(".fp-slides").length) {
-      g_interval = setInterval(function () {
-        setInterval(function () {
-          $.fn.fullpage.moveSlideRight();
-        }, 2000);
-      }, lapse);
-    }
-  },
-});
-
-$("#fullpage").fullpage({
-  anchors: ["s3"],
-  sectionsColor: ["#4285F4", "#EA4335", "#FBBC05", "#34A853"],
-});
-
-var startSlideSections = function () {};
-
-startSlideSections();
+console.log("hello");
